7e59910cd33732283a15c8de3629dd68da69dbd4
TiagoAle <tiago.alexandre@acad.ifce.edu.br>
2017-05-03 17:56:00 -0300

    AG com testes

--- AG.m
+++ AG.m
@@ -2,49 +2,40 @@ clc;
 clear all;
 close all;
 %Carregando dados da iris embaralhados
-global X
-global D
+global X_treino
+global D_treino
 B = load('iris.data');
 B = B(randperm(size(B,1)),:);
 
 bias = repmat(-1,size(B,1),1);
 
 B = [bias B];
+treinamento = B(1:120,:);
+teste = B(121:150,:);
 
-D = B(:,6);
-X = B(:,1:5);
+D_treino = treinamento(:,6);
+X_treino = treinamento(:,1:5);
 
-geracoes = 50;
-W = rand(20, 5);
-populacaoInicial = W;
+Y_teste = teste(:,6);
+X_teste = teste(:,1:5);
 
-% INITIALIZE THE NEURAL NETWORK PROBLEM %
-% inputs for the neural net
-inputs = X;
-% targets for the neural net
-targets = D;
 nvars = 5;
-% create handle to the MSE_TEST function, that
-% calculates MSE
-
-% Setting the Genetic Algorithms tolerance for
-% minimum change in fitness function before
-% terminating algorithm to 1e-8 and displaying
-% each iteration's results.
 %% Start with the default options
 options = gaoptimset;
 %% Modify options setting
+options = gaoptimset(options,'EliteCount', 4);
 options = gaoptimset(options,'CrossoverFcn', {  @crossoverheuristic [] });
 options = gaoptimset(options,'Display', 'off');
 options = gaoptimset(options,'PlotFcns', { @gaplotbestf });
 [x,fval,exitflag,output,population,score] = ...
 ga(@myFitness,nvars,[],[],[],[],[],[],[],[],options);
 
-Y_test = X * x';
-for i = 1:150
-    if(Y_test(i)> 0)
-        Y_test(i) = 1;
+Y_obt = X_teste * x';
+for i = 1:30
+    if(Y_obt(i)> 0)
+        Y_obt(i) = 1;
     else
-        Y_test(i) = 0;
+        Y_obt(i) = 0;
     end
 end
+mseTest = sum((Y_obt-Y_teste).^2)/length(Y_teste);
--- myFitness.m
+++ myFitness.m
@@ -1,14 +1,14 @@
 function mse_calc = myFitness(x)
-    global X
-    global D
-    y = X * x';
-    for i = 1:150
+    global X_treino
+    global D_treino
+    y = X_treino * x';
+    for i = 1:120
         if(y(i)> 0)
            y(i) = 1;
         else
            y(i) = 0;
         end
     end
-    mse_calc = sum((y-D).^2)/length(D);
+    mse_calc = sum((y-D_treino).^2)/length(D_treino);
 end
 
